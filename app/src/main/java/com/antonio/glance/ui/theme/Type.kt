5c1386501c0aebe1b85cf57541e9aff4b9c346de
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-18 18:49:37 +0100

    different typography styles specified in Type.kt. defo will be tweaked later on

--- app/src/main/java/com/antonio/glance/ui/theme/Type.kt
+++ app/src/main/java/com/antonio/glance/ui/theme/Type.kt
@@ -8,27 +8,52 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // bold text on card?
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    // bigger version of bold text?
+    titleMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.sp
+    ),
+    // even bigger version of bold text?
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    // smallest text
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Thin,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // 2nd smallest
+    labelMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.ExtraLight,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // 3rd smallest
+    labelLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
