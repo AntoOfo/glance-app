96edbd5012f15714fed71e30e1e437044db790d7
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-24 20:48:17 +0100

    file renamed. composable and preview created

---
+++ app/src/main/java/com/antonio/glance/screens/VerticalPager.kt
@@ -0,0 +1,18 @@
+package com.antonio.glance.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.antonio.glance.ui.theme.GlanceTheme
+
+@Composable
+fun NewsVerticalPager() {
+
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF808080)
+@Composable
+fun NewsVerticalPagerPreview() {
+    GlanceTheme {
+        NewsVerticalPager()
+    }
+}
