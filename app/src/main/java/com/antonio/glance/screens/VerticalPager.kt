5a8d93e86ff614034e3ab039c55f730fd3a3923d
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-24 21:27:09 +0100

    pager started, needs to fix space between each card and overall size

--- app/src/main/java/com/antonio/glance/screens/VerticalPager.kt
+++ app/src/main/java/com/antonio/glance/screens/VerticalPager.kt
@@ -1,12 +1,36 @@
 package com.antonio.glance.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.antonio.glance.ui.theme.GlanceTheme
 
 @Composable
-fun NewsVerticalPager() {
+fun NewsVerticalPager(modifier: Modifier = Modifier) {
+    val newsCount = 5   // dummy amount for now
 
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f  // null
+    ) {
+        newsCount
+    }
+    VerticalPager(
+        state = pagerState,
+        modifier = modifier.fillMaxSize(),
+    ) { pageIndex ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            NewsCard(modifier = Modifier)
+        }
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFF808080)
