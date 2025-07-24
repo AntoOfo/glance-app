333c80518a45c06ce387053379332292c19ca357
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-24 19:36:51 +0100

    working on searchbar ui

--- app/src/main/java/com/antonio/glance/screens/SearchBar.kt
+++ app/src/main/java/com/antonio/glance/screens/SearchBar.kt
@@ -1,13 +1,47 @@
 package com.antonio.glance.screens
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.antonio.glance.ui.theme.GlanceTheme
 
 @Composable
 fun SearchBar(modifier: Modifier = Modifier){
-
+    Surface(
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        TextField(
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            placeholder = {
+                Text(
+                    text = "Looking for news?",
+                    color = Color.Gray)
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFF808080)
