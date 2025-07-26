05a203ae8a700bef97cabc57aa560eb664b49654
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-26 21:46:17 +0100

    rough homescreen composable setup w/o navbar

--- app/src/main/java/com/antonio/glance/screens/HomeContent.kt
+++ app/src/main/java/com/antonio/glance/screens/HomeContent.kt
@@ -1,10 +1,26 @@
 package com.antonio.glance.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
 
+    Column(modifier = modifier) {
+        Spacer(Modifier.height(16.dp))
 
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+
+        NewsVerticalPager()
+    }
 }
+
+
+
+
+
