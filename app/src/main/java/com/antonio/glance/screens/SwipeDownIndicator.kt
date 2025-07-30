8982bcbb5e014739d3bd026327f5e9815a8f10c9
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-30 22:00:56 +0100

    swipe down indicator roughly created again, need to add to column

--- app/src/main/java/com/antonio/glance/screens/SwipeDownIndicator.kt
+++ app/src/main/java/com/antonio/glance/screens/SwipeDownIndicator.kt
@@ -1,13 +1,39 @@
 package com.antonio.glance.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.antonio.glance.ui.theme.GlanceTheme
 
 @Composable
 fun SwipeDownIndicator(modifier: Modifier = Modifier) {
-
+    Column(
+        modifier = modifier.padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowDown,
+            contentDescription = "Arrow Done One",
+            tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+            modifier = Modifier.size(24.dp)
+        )
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowDown,
+            contentDescription = "Arrow Done Two",
+            tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+            modifier = Modifier.offset(y = (-12).dp).size(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFF808080)
