81ef48894e9daadb0631b27b05c51fd306aee27b
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-30 21:45:24 +0100

    working on column

--- app/src/main/java/com/antonio/glance/screens/NewsColumn.kt
+++ app/src/main/java/com/antonio/glance/screens/NewsColumn.kt
@@ -1,2 +1,37 @@
 package com.antonio.glance.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.antonio.glance.ui.theme.GlanceTheme
+
+@Composable
+fun NewsColumn(modifier: Modifier = Modifier) {
+
+    Surface(
+        shape = MaterialTheme.shapes.large,
+        color = MaterialTheme.colorScheme.surface
+    ) {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            modifier = modifier
+        ) {
+            items(5) { index ->
+                NewsCard()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF808080)
+@Composable
+fun NewsColumnPreview() {
+    GlanceTheme {
+        NewsColumn()
+    }
+}
