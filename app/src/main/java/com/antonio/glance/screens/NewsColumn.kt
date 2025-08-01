dde5a4540e7fc0c6e5de511408a7070c32f24a78
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-01 22:11:24 +0100

    launched effect added for scrolling

--- app/src/main/java/com/antonio/glance/screens/NewsColumn.kt
+++ app/src/main/java/com/antonio/glance/screens/NewsColumn.kt
@@ -1,5 +1,6 @@
 package com.antonio.glance.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
@@ -9,12 +10,19 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonio.glance.ui.theme.GlanceTheme
+import kotlinx.coroutines.delay
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun NewsColumn(modifier: Modifier = Modifier) {
     // gives list of items for snappping
@@ -23,6 +31,24 @@ fun NewsColumn(modifier: Modifier = Modifier) {
     val flingBehavior = rememberSnapFlingBehavior(
         lazyListState = listState,
         snapPosition = SnapPosition.Start)  // snaps to top of column
+
+    var showIndicator by remember { mutableStateOf(true) }
+    var lastScrollTime by remember { mutableStateOf(0L) }
+
+    // detects scrolling
+    LaunchedEffect(listState.isScrollInProgress) {
+        if (listState.isScrollInProgress) {
+            showIndicator = false
+            lastScrollTime = System.currentTimeMillis()
+        } else {
+            // time left from 1500ms and scroll time
+            val timeDelay = 1500L - (System.currentTimeMillis() - lastScrollTime)
+            if (timeDelay > 0) {
+                delay(timeDelay)
+            }
+            showIndicator = true
+        }
+    }
 
         LazyColumn(
             state = listState,
