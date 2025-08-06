1b7bcdf76cc49252fc2e466d8106f4579e59953e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-06 19:58:15 +0100

    composable created to hold both portrait and landscape app versions

--- app/src/main/java/com/antonio/glance/screens/HomeContent.kt
+++ app/src/main/java/com/antonio/glance/screens/HomeContent.kt
@@ -1,5 +1,6 @@
 package com.antonio.glance.screens
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antonio.glance.ui.theme.GlanceTheme
@@ -66,6 +68,18 @@ fun MyAppLandscape() {
                 HomeScreen(showImage = false)
             }
         }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    if (isLandscape) {
+        MyAppLandscape()
+    } else {
+        MyAppPortrait()
     }
 }
 
--- app/src/main/java/com/antonio/glance/screens/SearchBar.kt
+++ app/src/main/java/com/antonio/glance/screens/SearchBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -21,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,7 +91,9 @@ fun CategoryRow(modifier: Modifier = Modifier) {
                 onClick = { selectedIndex = index },
                 selected = index == selectedIndex,
                 colors = SegmentedButtonDefaults.colors(
-                    activeContainerColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f)
+                    activeContainerColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f),
+                    inactiveBorderColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f),
+                    activeBorderColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f)
                 )
                     ) {
                         Text(label)
