28a5871495543d64b3ccdabc7b5110ddfaf712d8
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-06 20:00:40 +0100

    added MyApp composable to mainactivity

--- app/src/main/java/com/antonio/glance/MainActivity.kt
+++ app/src/main/java/com/antonio/glance/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.antonio.glance.screens.MyApp
 import com.antonio.glance.ui.theme.GlanceTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GlanceTheme {
-
+                MyApp()
             }
         }
     }
