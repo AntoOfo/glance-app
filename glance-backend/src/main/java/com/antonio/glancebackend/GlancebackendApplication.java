a9ffd089443f3e2e3440fe5f850f6444f3cb2fdc
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-06 20:22:05 +0100

    spring initializr files added to new glance-backend directory

---
+++ glance-backend/src/main/java/com/antonio/glancebackend/GlancebackendApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.glancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlancebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlancebackendApplication.class, args);
+	}
+
+}
