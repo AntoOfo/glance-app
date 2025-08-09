60a37ef4231ea95a5c301c117f62ba1ab47917af
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-09 17:05:54 +0100

    imports brought in

--- glance-backend/src/main/java/com/antonio/glancebackend/controller/NewsController.java
+++ glance-backend/src/main/java/com/antonio/glancebackend/controller/NewsController.java
@@ -3,6 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.antonio.glancebackend.controller;
+import org.springframework.beans.factory.annotation.value;
+import org.springframwork.web.bind.annotation.GetMapping;
+import org.springframwork.web.bind.annotation.RequestParam;
+import org.springframwork.web.bind.annotation.RestController;
+import org.springframwork.web.reactive.function.client.WebClient;
 
 /**
  *
