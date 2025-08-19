dd3944189d4811b67c260844b24c2a431cee91bf
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-19 18:23:45 +0100

    module set up

--- app/src/main/java/com/antonio/glance/room/AppModule.kt
+++ app/src/main/java/com/antonio/glance/room/AppModule.kt
@@ -1,2 +1,27 @@
 package com.antonio.glance.room
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context) : ArticleDatabase =
+        Room.databaseBuilder(
+            context,
+            ArticleDatabase::class.java,
+            "glance_db"
+        ).build()
+
+    @Provides
+    fun provideArticleDao(db: ArticleDatabase): ArticleDao = db.articleDao()
+}
