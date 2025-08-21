542601cc7a856a39d85d042d5c00aae5be750b53
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-21 21:22:24 +0100

    animation tracker and scale values set for icon scale animation

--- app/src/main/java/com/antonio/glance/screens/NewsCard.kt
+++ app/src/main/java/com/antonio/glance/screens/NewsCard.kt
@@ -2,6 +2,8 @@ package com.antonio.glance.screens
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -28,6 +30,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -100,6 +106,14 @@ fun NewsCard(
    // }
     val context = LocalContext.current
     val haptic = LocalHapticFeedback.current
+
+    var trackAnim by remember { mutableStateOf(false) }
+
+    val scale by animateFloatAsState(
+        targetValue = if (trackAnim) 1.4f else 1f,
+        animationSpec = tween(durationMillis = 150),
+        label = "bookmarkScale"
+    )
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
